7e239f3e6d64df34badf4f1f44118f78d5cd25db
Vuong <vuongbachdoan@gmail.com>
2024-10-08 02:29:48 +0700

    fix: fix pipeline

--- scripts/start_docker_container.sh
+++ scripts/start_docker_container.sh
@@ -4,9 +4,9 @@
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 575108955981.dkr.ecr.us-west-2.amazonaws.com
 
 # Kill any process using port 3000
-fuser -k 3000/tcp
+sudo lsof -t -i tcp:3000 | xargs -r kill -9
 
-# Stop the existing Docker container
+# Stop the existing Docker container if it's running
 docker stop cicd-demo
 
 # Remove the existing Docker container
