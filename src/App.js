eb8232c306232cd43648f2ae83bfab459f0fbd50
Vuong <vuongbachdoan@gmail.com>
2024-10-08 02:40:37 +0700

    fix: fix pipeline

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Vuong Dep Trai
+          Chao Cac May
         </p>
         <a
           className="App-link"
