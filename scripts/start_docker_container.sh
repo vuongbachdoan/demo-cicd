09800e750a3b2cbfc315fadc35388da854a9e2d5
Vuong <vuongbachdoan@gmail.com>
2024-10-08 00:52:42 +0700

    feat: add source

--- scripts/start_docker_container.sh
+++ scripts/start_docker_container.sh
@@ -3,9 +3,13 @@
 # Log in to ECR
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 575108955981.dkr.ecr.us-west-2.amazonaws.com
 
-# Pull the Docker image
-docker pull 575108955981.dkr.ecr.us-west-2.amazonaws.com/vuongbach/cicd-demo:latest
+# Check if the container is already running
+if [ $(docker ps -q -f name=cicd-demo) ]; then
+    echo "Stopping existing container..."
+    docker stop cicd-demo
+    echo "Removing existing container..."
+    docker rm cicd-demo
+fi
 
-# Run the Docker container (adjust as needed)
+# Run the Docker container
 docker run -d --name cicd-demo -p 3000:3000 575108955981.dkr.ecr.us-west-2.amazonaws.com/vuongbach/cicd-demo:latest
-
