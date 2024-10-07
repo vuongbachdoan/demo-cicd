e8ec4ee986b39ae93538b3a534ebdb39ae97fac0
Vuong <vuongbachdoan@gmail.com>
2024-10-08 00:33:19 +0700

    feat: add source

--- scripts/start_docker_container.sh
+++ scripts/start_docker_container.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-docker run -d -p 80:80 --name my-react-app 575108955981.dkr.ecr.us-west-2.amazonaws.com/vuongbach/cicd-demo:latest
+
+# Log in to ECR
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 575108955981.dkr.ecr.us-west-2.amazonaws.com
+
+# Pull the Docker image
+docker pull 575108955981.dkr.ecr.us-west-2.amazonaws.com/vuongbach/cicd-demo:latest
+
+# Run the Docker container (adjust as needed)
+docker run -d -p 3000:3000 575108955981.dkr.ecr.us-west-2.amazonaws.com/vuongbach/cicd-demo:latest
